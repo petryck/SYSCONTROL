956c3157dc591de8607d78b26003f2dcfc0c3aac
Petryck WIlliam <petryck@i9devgroup.com>
2022-04-18 22:28:08 -0300

    .
    .

--- server/server.js
+++ server/server.js
@@ -25,13 +25,14 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 
 var connection = mysql.createConnection({
-  host: "ads-con.csvfil6euj3s.sa-east-1.rds.amazonaws.com",
-  user: "ads",
-  port: "3286",
-  password: "99659819aA",
-  database: "db",
+  host: "144.22.225.253",
+  user: "aplicacao",
+  port: "3306",
+  password: "conline@2510A",
+  database: "SIRIUS",
   charset: "utf8mb4"
 });
+
 
 
 connection.connect(function(err) {
@@ -123,37 +124,130 @@ app.get('/app_janela', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/janelas/'+req.query.pagina+'.html'))
 })
 
+app.post('/QueryListaEstoque', function (req, res) {
+  
+  var arrayLiteral2 = [];
 
+  var sql = `SELECT ANALUA.estoque.idEstoque as IdEstoque,
+            ANALUA.categorias.nome as NomeCategoria,
+            ANALUA.modelo.nome as NomeModelo,
+            ANALUA.estoque.modelo as IdModelo,
+            ANALUA.tamanho.nome as NomeTamanho,
+            ANALUA.tamanho.idTamanho as IdTamanho,
+            ANALUA.cores.nome as NomeCor,
+            ANALUA.cores.idCores as IdCor,
+            (SELECT SUM(ANALUA.estoque.quantidade) FROM ANALUA.estoque 
+            WHERE ANALUA.estoque.cor = IdCor
+            AND ANALUA.estoque.tamanho = IdTamanho
+            AND ANALUA.estoque.modelo = IdModelo) as Quantidade
+            FROM ANALUA.estoque
+        INNER JOIN ANALUA.modelo ON ANALUA.modelo.id_modelo = ANALUA.estoque.modelo
+        INNER JOIN ANALUA.categorias ON ANALUA.modelo.categoria = ANALUA.categorias.id_categoria
+        INNER JOIN ANALUA.tamanho ON ANALUA.tamanho.idTamanho = ANALUA.estoque.tamanho
+        INNER JOIN ANALUA.cores ON ANALUA.cores.idCores = ANALUA.estoque.cor
+        GROUP BY IdTamanho,IdCor,IdModelo
 
-
-app.post('/cotacao_moedas_hoje', function (req, res) {
-  let id = req.query.id;
-  var data = new Date();
-  var dia  = data.getDate().toString().padStart(2, '0');
-  var mes  = (data.getMonth()+1).toString().padStart(2, '0'); //+1 pois no getMonth Janeiro começa com zero.
-  var ano  = data.getFullYear();
-
-  var datainicio = mes+'-'+dia+'-'+ano;
- 
-
-  var sql = `SELECT moedas.id as MoedaId,
-              moedas.id as MoedaId,
-              moedas.simbolo as MoedaSimbolo,
-              cotacao_moedas.abertura as MoedaAbertura
-              FROM vz_moedas 
-              JOIN moedas ON moedas.id = vz_moedas.moeda
-              JOIN cotacao_moedas ON moedas.id = cotacao_moedas.moeda
-              WHERE cotacao_moedas.data = '${datainicio}'`;
+`;
 
   connection.query(sql, function(err2, results){
 
-       res.json(results);
-  })
+              
+    results.forEach(e => {
 
-});
+      var objeto = {
+        id: e.IdEstoque,
+        nome: e.NomeModelo,
+        categoria: e.NomeCategoria,
+        tamanho: e.NomeTamanho,
+        cor: e.NomeCor,
+        quantidade: e.Quantidade
+        
+    }
+  
+          arrayLiteral2.push(objeto);
+        })
+
+        let saida = {
+          "draw": 1,
+          "recordsTotal": results.length,
+          "recordsFiltered": results.length,
+          "data": arrayLiteral2
+        } 
+
+
+        res.json(saida)
+
+      })
+})
+app.post('/QueryPessoasVisitantes', function (req, res) {
+
+  var arrayLiteral2 = [];
+
+  var sql = `SELECT * FROM SIRIUS.cad_visitantes`;
+
+
+  
+          connection.query(sql, function(err2, results){
+              
+            results.forEach(e => {
+
+
+              var btn = ``;
+
+
+             var data_entrou = new Date(parseInt(e.data_cadastro));
+      
+              var dia  = data_entrou.getDate().toString().padStart(2, '0');
+              var mes  = (data_entrou.getMonth()+1).toString().padStart(2, '0'); //+1 pois no getMonth Janeiro começa com zero.
+              var ano  = data_entrou.getFullYear();
+
+              var hour   = data_entrou.getHours();
+              var minute = data_entrou.getMinutes();
+              var ap = "AM";
+              if (hour   > 11) { ap = "PM";             }
+              if (hour   > 12) { hour = hour - 12;      }
+              if (hour   == 0) { hour = 12;             }
+              if (hour   < 10) { hour   = "0" + hour;   }
+              if (minute < 10) { minute = "0" + minute; }
+            
+            
+            
+              var data_cadastro = dia+'-'+mes+'-'+ano+' '+hour + ':' + minute + ' ' + ap;
+
+           
+              
+              
+
+             var objeto = {
+                id: e.id_cad_visitantes,
+                nome: e.nome,
+                documento: e.documento,
+                data_cadastro: data_cadastro,
+                acao: btn
+                
+            }
+          
+
+            
 
 
 
+            arrayLiteral2.push(objeto);
+            })
+            
+            
+            
+            let saida = {
+              "draw": 1,
+              "recordsTotal": results.length,
+              "recordsFiltered": results.length,
+              "data": arrayLiteral2
+            } 
+
+
+            res.json(saida)
+          })
+})
 
 
 server.listen(port, function () {
